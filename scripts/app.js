deddb52917f674fe75e3de0c3646a32dc9888461
na3ap4uk <na3ap4uk@ukr.net>
2016-04-03 19:59:36 +0300

    newFalligObject function added, improved update function

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-define(['components/models/hero', 'components/models/canvas', 'components/models/fallobject', 'components/functions/randomX', 'components/functions/randomY', 'components/models/score', 'components/models/lives', 'components/functions/clearCanvas', 'components/functions/menuSound', 'components/functions/gameOverSound', 'components/functions/gameSound', 'components/models/menu', 'components/functions/touchBraSound', 'components/functions/touchCactusSound', 'components/functions/gameOverMenu', 'components/models/girls'], function(hero, Canvas, ball, randomX, randomY, score, lives, clearCanvas, menuSound, gameOverSound, gameSound, menu, touchBraSound, touchCactusSound, gameOverMenu, girls) {
+define(['components/models/hero', 'components/models/canvas', 'components/models/fallobject', 'components/functions/randomX', 'components/functions/randomY', 'components/models/score', 'components/models/lives', 'components/functions/clearCanvas', 'components/functions/menuSound', 'components/functions/gameOverSound', 'components/functions/gameSound', 'components/models/menu', 'components/functions/touchBraSound', 'components/functions/touchCactusSound', 'components/functions/gameOverMenu','components/functions/newFallingElement', 'components/models/girls'], function(hero, Canvas, ball, randomX, randomY, score, lives, clearCanvas, menuSound, gameOverSound, gameSound, menu, touchBraSound, touchCactusSound, gameOverMenu, newFallingElement, girls) {
     "use strict";
     // gameOverMenu();
     menu();
@@ -8,7 +8,6 @@ define(['components/models/hero', 'components/models/canvas', 'components/models
         H = Canvas.canvas.height;
 
     function update() {
-        var randomNumber = Math.floor((Math.random() * 2) + 1);
         clearCanvas();
         score.draw();
         lives.draw();
@@ -18,48 +17,18 @@ define(['components/models/hero', 'components/models/canvas', 'components/models
         ball.y += ball.vy;
 
         if (ball.y + ball.vy > H - ball.radius || ball.y + ball.vy < ball.radius) {
-            ball.x = randomX();
-            ball.y = randomY();
-            girls.x = ball.x;
-            girls.y = ball.y;
-            if (randomNumber === 1) {
-                ball.color = "red";
-            } else {
-                ball.color = "#0095DD";
-            }
-            girls.draw();
-            ball.draw();
+            newFallingElement();
         }
 
         if ((ball.y) > (hero.Y - hero.height) && (ball.x + ball.radius) > hero.X && (ball.x - ball.radius) < (hero.X + hero.width) && ball.color == "red") {
             score.count++;
-            ball.x = randomX();
-            ball.y = randomY();
-            girls.x = ball.x;
-            girls.y = ball.y;
-            if (randomNumber === 1) {
-                ball.color = "red";
-            } else {
-                ball.color = "#0095DD";
-            }
-            girls.draw();
-            ball.draw();
+            newFallingElement();
             touchBraSound();
         }
 
         if ((ball.y) > (hero.Y - hero.height) && (ball.x + ball.radius) > hero.X && (ball.x - ball.radius) < (hero.X + hero.width) && ball.color == "#0095DD") {
             lives.count--;
-            ball.x = randomX();
-            ball.y = randomY();
-            girls.x = ball.x;
-            girls.y = ball.y;
-            if (randomNumber === 1) {
-                ball.color = "red";
-            } else {
-                ball.color = "#0095DD";
-            }
-            girls.draw();
-            ball.draw();
+            newFallingElement();
             touchCactusSound();
         }
 
--- scripts/components/functions/newFallingElement.js
+++ scripts/components/functions/newFallingElement.js
@@ -1,18 +1,19 @@
-define(['components/models/canvas'], function(Canvas) {
+define(['components/models/canvas', 'components/models/fallobject', 'components/models/girls','components/functions/randomX', 'components/functions/randomY'], function(Canvas, ball, girls, randomX, randomY) {
     "use strict";
+    var randomNumber = Math.floor((Math.random() * 2) + 1);
 
     function newFallingElement() {
-            ball.x = randomX();
-            ball.y = randomY();
-            girls.x = ball.x;
-            girls.y = ball.y;
-            if (randomNumber === 1) {
-                ball.color = "red";
-            } else {
-                ball.color = "#0095DD";
-            }
-            girls.draw();
-            ball.draw();
-            }
+        ball.x = randomX();
+        ball.y = randomY();
+        girls.x = ball.x;
+        girls.y = ball.y;
+        if (randomNumber === 1) {
+            ball.color = "red";
+        } else {
+            ball.color = "#0095DD";
+        }
+        girls.draw();
+        ball.draw();
+    }
     return newFallingElement;
 });
