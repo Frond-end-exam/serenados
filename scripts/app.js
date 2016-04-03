36cea76f62e23b3ac17f3fdc1316abdfabf49861
na3ap4uk <na3ap4uk@ukr.net>
2016-04-03 22:02:56 +0300

    heart falling created, needs fixing bags

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,8 @@
-define(['components/models/hero', 'components/models/canvas', 'components/models/fallobject', 'components/functions/randomX', 'components/functions/randomY', 'components/models/score', 'components/models/lives', 'components/functions/clearCanvas', 'components/functions/menuSound', 'components/functions/gameOverSound', 'components/functions/gameSound', 'components/models/menu', 'components/functions/touchBraSound', 'components/functions/touchCactusSound', 'components/functions/gameOverMenu','components/functions/newFallingElement', 'components/models/girls'], function(hero, Canvas, ball, randomX, randomY, score, lives, clearCanvas, menuSound, gameOverSound, gameSound, menu, touchBraSound, touchCactusSound, gameOverMenu, newFallingElement, girls) {
+define(['components/models/hero', 'components/models/canvas', 'components/models/fallobject', 'components/functions/randomX', 'components/functions/randomY', 'components/models/score', 'components/models/lives', 'components/functions/clearCanvas', 'components/functions/menuSound', 'components/functions/gameOverSound', 'components/functions/gameSound', 'components/models/menu', 'components/models/heart', 'components/functions/touchBraSound', 'components/functions/touchCactusSound', 'components/functions/gameOverMenu', 'components/functions/newFallingElement', 'components/models/girls'], function(hero, Canvas, ball, randomX, randomY, score, lives, clearCanvas, menuSound, gameOverSound, gameSound, menu, heart, touchBraSound, touchCactusSound, gameOverMenu, newFallingElement, girls) {
     "use strict";
     // gameOverMenu();
     menu();
     menuSound();
-
     var W = Canvas.canvas.width,
         H = Canvas.canvas.height;
 
@@ -15,30 +14,29 @@ define(['components/models/hero', 'components/models/canvas', 'components/models
         ball.draw();
         hero.draw();
         ball.y += ball.vy;
-
         if (ball.y + ball.vy > H - ball.radius || ball.y + ball.vy < ball.radius) {
             newFallingElement();
         }
-
         if ((ball.y) > (hero.Y - hero.height) && (ball.x + ball.radius) > hero.X && (ball.x - ball.radius) < (hero.X + hero.width) && ball.color == "red") {
             score.count++;
             newFallingElement();
             touchBraSound();
         }
-
         if ((ball.y) > (hero.Y - hero.height) && (ball.x + ball.radius) > hero.X && (ball.x - ball.radius) < (hero.X + hero.width) && ball.color == "#0095DD") {
             lives.count--;
             newFallingElement();
             touchCactusSound();
         }
-
+        if (score.count === 1) {
+            heart.draw();
+            heart.y += heart.vy;
+        }
         if (lives.count === 0) {
             clearInterval(gameInterval);
             gameOverSound();
             gameOverMenu();
         }
     }
-
     var gameInterval = setInterval(function() {
         update();
     }, 10);
