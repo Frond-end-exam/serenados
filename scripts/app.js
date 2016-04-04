c60233abb104a66e2e7d8e53aabf7622c06fd22e
lanzeron <lanzeron@gmail.com>
2016-04-04 14:40:59 +0300

    fixed mute buttom

--- scripts/app.js
+++ scripts/app.js
@@ -2,27 +2,25 @@
 define(['components/models/hero', 'components/models/canvas', 'components/models/fallobject', 'components/functions/randomX', 'components/functions/randomY', 'components/models/score', 'components/models/lives', 'components/functions/clearCanvas', 'components/functions/menuSound', 'components/functions/gameOverSound', 'components/functions/gameSound', 'components/models/menu', 'components/models/heart', 'components/functions/touchBraSound', 'components/functions/touchCactusSound', 'components/functions/gameOverMenu', 'components/functions/newFallingElement', 'components/functions/bonusLife', 'components/models/girls', 'components/models/canvasWidth', 'components/models/canvasHeight'], function(hero, Canvas, ball, randomX, randomY, score, lives, clearCanvas, menuSound, gameOverSound, gameSound, menu, heart, touchBraSound, touchCactusSound, gameOverMenu, newFallingElement, bonusLife, girls, W, H) {
 
     "use strict";
-    // gameOverMenu();
     menu();
     menuSound();
 
 
 
     $('#start').click(function() {
-
+        var score = 0,
+            lives = 3;
 
         $('#menu').css('display', 'none');
-        $('#main').css('display', 'block');
+        $('.game').css('display', 'block');
         gameSound();
         var gameInterval = setInterval(function update() {
-      $(".lives3").text(lives.count);
-      $(".scoreCount").text(score.count);
+      $(".lives3").text(lives);
+      $(".scoreCount").text(score);
         var randomNumber = Math.floor((Math.random() * 2) + 1);
 
 
         clearCanvas();
-        score.draw();
-        lives.draw();
         girls.draw();
         ball.draw();
         hero.draw();
@@ -31,21 +29,21 @@ define(['components/models/hero', 'components/models/canvas', 'components/models
             newFallingElement();
         }
         if ((ball.y) > (hero.Y - hero.height) && (ball.x + ball.radius) > hero.X && (ball.x - ball.radius) < (hero.X + hero.width) && ball.color == "red") {
-            score.count++;
+            score++;
 
 
             newFallingElement();
             touchBraSound();
         }
         if ((ball.y) > (hero.Y - hero.height) && (ball.x + ball.radius) > hero.X && (ball.x - ball.radius) < (hero.X + hero.width) && ball.color == "#0095DD") {
-            lives.count--;
+            lives--;
             newFallingElement();
             touchCactusSound();
         }
-        if (score.count > 1 && score.count % 10 === 0) {
+        if (score > 1 && score % 10 === 0) {
             bonusLife();
         }
-        if (lives.count === 0) {
+        if (lives === 0) {
             clearInterval(gameInterval);
             setTimeout(function() {
                 window.location.reload();
