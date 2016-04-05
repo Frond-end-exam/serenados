7c9c118fcb70a2043430c574d11d966beae4e3ae
na3ap4uk <na3ap4uk@ukr.net>
2016-04-06 00:04:14 +0300

    changes before merge

--- scripts/components/functions/changeDayNight.js
+++ scripts/components/functions/changeDayNight.js
@@ -2,25 +2,21 @@ define(['components/models/canvas'], function(Canvas) {
     "use strict";
 
     function changeDayNight() {
-        if ($('#main').hasClass('day')) {
-            $('#main').removeClass('day').addClass('evening');
-            $('#canvas').css("background-image", "url("../assets/images/backgrounds/eveningBuilding.gif")");
+        if ($('#main').hasClass('.day')) {
+            $('#main').removeClass('.day').addClass('.evening');
+            $('#canvas').removeClass('.canvasEvening').addClass('.canvasNight');
         }
-
-
-
-
-        if ($('#main').hasClass('evening') && $('#canvas').hasClass('canvasEvening')) {
-            $('#main').removeClass('evening').addClass('night');
-            $('#canvas').removeClass('canvasEvening').addClass('canvasNight');
+        if ($('#main').hasClass('.evening')) {
+            $('#main').removeClass('.evening').addClass('.night');
+            $('#canvas').removeClass('.canvasEvening').addClass('.canvasNight');
         }
-        if ($('#main').hasClass('night') && $('#canvas').hasClass('canvasNight')) {
-            $('#main').removeClass('night').addClass('morning');
-            $('#canvas').removeClass('canvasNight').addClass('canvasMorning');
+        if ($('#main').hasClass('.night')) {
+            $('#main').removeClass('.night').addClass('.morning');
+            $('#canvas').removeClass('.canvasNight').addClass('.canvasMorning');
         }
-        if ($('#main').hasClass('morning') && $('#canvas').hasClass('canvasMorning')) {
-            $('#main').removeClass('morning').addClass('day');
-            $('#canvas').removeClass('canvasMorning').addClass('canvasDay');
+        if ($('#main').hasClass('.morning')) {
+            $('#main').removeClass('.morning').addClass('.day');
+            $('#canvas').removeClass('.canvasMorning').addClass('.canvasDay');
         }
     }
     return changeDayNight;
