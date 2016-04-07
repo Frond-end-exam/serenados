dc2ab5c10f879b81eac95f57e7ea75074b1b466e
VasylivIaroslav <jerry_lviv@yahoo.com>
2016-04-07 18:47:44 +0300

    added authors

--- scripts/components/models/menu.js
+++ scripts/components/models/menu.js
@@ -9,6 +9,12 @@ define(['../functions/gameSound'], function(gameSound) {
         $('#controls-description').click(function() {
             $('#controls-description').hide();
         });
+        $('#buttonAuthors').click(function() {
+            $('#authors-description').show();
+        });
+        $('#authors-description').click(function() {
+            $('#authors-description').hide();
+        });
         $('#mute').click(function() {
             soundManager.mute();
             $('#mute').hide();
