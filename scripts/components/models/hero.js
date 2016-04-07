a6a699e06e02f5c8b28c6a0c9e00be29eb963291
na3ap4uk <na3ap4uk@ukr.net>
2016-04-07 22:09:02 +0300

    hero turning on keydown

--- scripts/components/models/hero.js
+++ scripts/components/models/hero.js
@@ -20,61 +20,10 @@ define(['components/models/canvas'], function(Canvas) {
             leftPressed = false;
         }
     }
-    //-- ANIMATION --
-    /*
-    var coin,
-        coinImage,
-        canvas;                 
-
-    
-      coin.update();
-      coin.render();
-    
-    
-    function sprite (options) {
-    
-        
-        that.context = options.context;
-        that.width = options.width;
-        that.height = options.height;
-        that.image = options.image;
-        
-        
-          // Draw the animation
-          that.context.drawImage(
-            that.image,
-            frameIndex * that.width / numberOfFrames,
-            0,
-            that.width / numberOfFrames,
-            that.height,
-            0,
-            0,
-            that.width / numberOfFrames,
-            that.height);
-        }
-        
-        return that;
-    }
-    
-    // Create sprite sheet
-    
-    coinImage = new Image();    
-    
-    // Create sprite
-    coin = sprite({
-        context: canvas.getContext("2d"),
-        width: 1000,
-        height: 100,
-        image: coinImage,
-        numberOfFrames: 10,
-        ticksPerFrame: 4
-    });
-    
-    // Load sprite sheet
-    coinImage.addEventListener("load", gameLoop);
-    coinImage.src = "assets/images/moving-units/moving man/img-man.png";
-*/
-    //--------------------------
+    var imgLeft = new Image();
+    var imgRight = new Image();
+    imgLeft.src = "assets/images/moving-units/img-man-right.png";
+    imgRight.src = "assets/images/moving-units/img-man-left.png";
     var hero = {
         height: 30,
         width: 70,
@@ -82,15 +31,17 @@ define(['components/models/canvas'], function(Canvas) {
         Y: Canvas.canvas.height - 140,
         draw: function() {
             Canvas.ctx.beginPath();
-            $(hero).addClass('hero');
-            //ctx.rect(this.height, this.width);
-            //Canvas.ctx.drawImage(hero, this.X, this.Y);
+            if (rightPressed === true) {
+                Canvas.ctx.drawImage(imgRight, this.X, this.Y);
+            } else {
+                Canvas.ctx.drawImage(imgLeft, this.X, this.Y);
+            }
             Canvas.ctx.fill();
             Canvas.ctx.closePath();
             if (rightPressed && this.X < Canvas.canvas.width - this.width) {
-                this.X += 3;
+                this.X += 2;
             } else if (leftPressed && this.X > 0) {
-                this.X -= 3;
+                this.X -= 2;
             }
         }
     };
