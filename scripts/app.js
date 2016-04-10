7735de2b092770853e0fc8332bf6578d3482c717
na3ap4uk <na3ap4uk@ukr.net>
2016-04-10 16:59:46 +0300

    renamed classes in css

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
-define(['components/models/hero', 'components/models/canvas', 'components/models/fallobject', 'components/functions/randomX', 'components/functions/randomY', 'components/functions/clearCanvas', 'components/functions/menuSound', 'components/functions/gameOverSound', 'components/functions/gameSound', 'components/models/menu', 'components/models/heart', 'components/functions/touchBraSound', 'components/functions/touchCactusSound', 'components/functions/gameOverMenu', 'components/functions/newFallingElement', 'components/functions/bonusLife', 'components/models/girls', 'components/models/canvasWidth', 'components/models/canvasHeight', 'components/models/score', 'components/models/lives', 'components/functions/changeDayNight', 'components/models/angryMan', 'components/models/tv', 'components/functions/newFallingTV', 'components/functions/checkCoords', 'components/functions/touchTvSound', 'components/functions/newFallingRugbyBall', 'components/models/rugbyBall', 'components/models/rugbyPlayer', 'components/functions/touchBallSound'], function(hero, Canvas, ball, randomX, randomY, clearCanvas, menuSound, gameOverSound, gameSound, menu, heart, touchBraSound, touchCactusSound, gameOverMenu, newFallingElement, bonusLife, girls, W, H, score, lives, changeDayNight, angryMan, tv, newFallingTV, checkCoords, touchTvSound, newFallingRugbyBall, rugbyBall, rugbyPlayer, touchBallSound) {
+define(['components/models/hero', 'components/models/canvas', 'components/models/fallobject', 'components/functions/randomX', 'components/functions/randomY', 'components/functions/clearCanvas', 'components/functions/menuSound', 'components/functions/gameOverSound', 'components/functions/gameSound', 'components/models/menu', 'components/models/heart', 'components/functions/touchBraSound', 'components/functions/touchCactusSound', 'components/functions/gameOverMenu', 'components/functions/newFallingElement', 'components/functions/bonusLife', 'components/models/girls', 'components/models/canvasWidth', 'components/models/canvasHeight', 'components/models/score', 'components/models/lives', 'components/functions/changeDayNight', 'components/models/angryMan', 'components/models/tv', 'components/functions/newFallingTV', 'components/functions/checkCoords', 'components/functions/touchTvSound'], function(hero, Canvas, ball, randomX, randomY, clearCanvas, menuSound, gameOverSound, gameSound, menu, heart, touchBraSound, touchCactusSound, gameOverMenu, newFallingElement, bonusLife, girls, W, H, score, lives, changeDayNight, angryMan, tv, newFallingTV, checkCoords, touchTvSound) {
     "use strict";
+    
     menu();
     menuSound();
     $('#start').click(function() {
@@ -35,7 +36,7 @@ define(['components/models/hero', 'components/models/canvas', 'components/models
             if (score.count > 1 && score.count % 10 === 0) {
                 bonusLife();
             }
-            if (score.count > 20) {
+            if (score.count > 25) {
                 tv.y += tv.vy;
                 tv.draw();
                 angryMan.draw();
@@ -50,23 +51,6 @@ define(['components/models/hero', 'components/models/canvas', 'components/models
                 newFallingTV();
                 touchTvSound();
                 checkCoords();
-            }
-            if (score.count > 50) {
-                rugbyBall.y += rugbyBall.vy;
-                rugbyBall.x += rugbyBall.vx;
-                rugbyBall.draw();
-                rugbyPlayer.draw();
-                checkCoords();
-            }
-            if (rugbyBall.y + rugbyBall.vy > H - rugbyBall.radius || rugbyBall.y + rugbyBall.vy < rugbyBall.radius) {
-                newFallingRugbyBall();
-                checkCoords();
-            }
-            if ((rugbyBall.y) > (hero.Y - hero.height) && (rugbyBall.x + rugbyBall.radius) > hero.X && (rugbyBall.x - rugbyBall.radius) < (hero.X + hero.width)) {
-                lives.count--;
-                newFallingRugbyBall();
-                checkCoords();
-                touchBallSound();
             }
             if (lives.count === 0) {
                 $(".lives3").text(0);
